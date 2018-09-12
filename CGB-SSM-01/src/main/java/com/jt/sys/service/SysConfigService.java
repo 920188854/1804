ba6e6bbb993b3ecc83cf23bb013cc9aabe0ca35a
920188854 <920188854@qq.com>
2018-09-12 15:16:32 +0800

    rm

--- CGB-SSM-01/src/main/java/com/jt/sys/service/SysConfigService.java
+++
@@ -1,9 +0,0 @@
-package com.jt.sys.service;
-
-import com.jt.common.vo.PageObject;
-import com.jt.sys.entity.SysConfig;
-
-public interface SysConfigService {
-
-	PageObject<SysConfig> findPageObjects(String name, Integer pageCurrent);
-}
